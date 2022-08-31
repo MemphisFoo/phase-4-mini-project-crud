ddb375b62c1da21834a555cde9f537c8b046d059
Christian TDN <cjwtdn@gmail.com>
2022-08-30 21:07:18 -0600

    commit tree cacee8a841abb5dd7136175d4769fdc98a4b58f1

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,45 @@
+class SpicesController < ApplicationController
+    #GET spices/
+    def index
+        spices=Spice.all
+        render json: spices
+    end
+
+    #GET spices/:id
+    def show
+        spice=find_spice
+        render json:spice
+    end
+
+    #POST spices/
+    def create
+        spice=Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    #Patch spices/:id
+
+    def update
+        spice=find_spice
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice=find_spice
+        spice.destroy
+        head :no_content
+    end
+
+        
+    private
+   def find_spice
+        Spice.find_by(id: params[:id])   
+   end 
+   
+   def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+   end
+
+end
+
